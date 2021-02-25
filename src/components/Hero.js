1bcd86f0089f7c4b8fb0a9e70dc562eea370678e
Retro Future Music <JoshuaSohn@joshuas-mac-pro.home>
2021-02-25 13:57:39 -0500

    [updates] home page to play music video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,29 +9,11 @@ function Hero(props) {
       {/* <Link id="hero-link" to="/?songLink=strangers">
         <img id="Hero-img" src={process.env.PUBLIC_URL + image}></img>
       </Link> */}
-    <div
-      className="video"
-      style={{
-        position: "relative",
-        width: "100%",
-        display: "block",
-        paddingBottom: "0%" /* 16:9 */,
-        height: "90vh"
-      }}
-    >
-      <iframe
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%"
-        }}
-        src={`https://player.vimeo.com/video/467827976?autoplay=1&muted=1&loop=1ß`}
-        frameBorder="0"
-        allow="autoplay; fullscreen"
-      />
-    </div>
+      <div className="video-background">
+        <div className="video-foreground">
+          <iframe src="https://www.youtube.com/embed/qM5kHFOGgvg?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
     </div>
   );
 }
