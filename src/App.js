68901e763b9ead353a1464944bdd976f8523b5da
Retro Future Music <JoshuaSohn@Mac.lan>
2026-07-08 22:23:49 -0400

    Refresh City Princess landing page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,72 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 
-import Home from './components/Home';
-import StreamingLinks from './components/StreamingLinks'
+const musicLinks = [
+  {
+    label: 'Spotify',
+    href: 'https://open.spotify.com/artist/3ylFTwTwvZK33nHxIq56jS'
+  },
+  {
+    label: 'Apple Music',
+    href: 'https://music.apple.com/us/artist/city-princess/1546293600'
+  },
+  {
+    label: 'YouTube',
+    href: 'https://www.youtube.com/channel/UC2oSdSzYYnuAhBF87vuBO1w'
+  },
+  {
+    label: 'Amazon Music',
+    href: 'https://music.amazon.com/artists/B08Q757LPN/city-princess'
+  },
+  {
+    label: 'Tidal',
+    href: 'https://tidal.com/artist/22573294'
+  }
+];
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={getSongFromQueryString}>
-        </Route>
-      </Switch>
-    </Router>
+    <main className="page-shell">
+      <section className="release" aria-labelledby="release-title">
+        <div className="art-wrap">
+          <img
+            className="cover-art"
+            src={`${process.env.PUBLIC_URL}/cant-love-you-anymore-art.jpg`}
+            alt="Can't love you anymore cover art"
+          />
+        </div>
+
+        <div className="release-copy">
+          <p className="artist">City Princess</p>
+          <h1 id="release-title">can't love you anymore</h1>
+        </div>
+
+        <nav className="link-list" aria-label="Music services">
+          {musicLinks.map((link) => (
+            <a
+              className="service-link"
+              href={link.href}
+              key={link.label}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>{link.label}</span>
+              <span aria-hidden="true">Open</span>
+            </a>
+          ))}
+        </nav>
+
+        <a
+          className="instagram-link"
+          href="https://www.instagram.com/_city_princess_/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @_city_princess_
+        </a>
+      </section>
+    </main>
   );
 }
-
-const validSongLinkParams = ['strangers', 'behere'];
-
-function getSongFromQueryString(props) {
-  const song = new URLSearchParams(props.location.search).get('songLink')
-  if (validSongLinkParams.includes(song)) {
-    return (<StreamingLinks {...props} song={song} />)
-  } else {
-    return (<Home />)
-  }
-}
-
 
 export default App;
